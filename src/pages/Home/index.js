e187be48ca38b92d903b0fc6b50db6cb4efa6e0f
noorqidam <noorqidam@gmail.com>
2020-12-24 14:42:25 +0700

    add Pages Home

---
+++ src/pages/Home/index.js
@@ -0,0 +1,135 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import {
+  CardProduct,
+  InputText,
+  LayoutSidebar,
+  Pagination,
+  Pill,
+  Responsive,
+  SideNav,
+} from "upkit";
+import {
+  fetchProducts,
+  goToNextPage,
+  goToPrevPage,
+  setCategory,
+  setKeyword,
+  setPage,
+  toggleTag,
+} from "../../features/Products/actions";
+import { addItem, removeItem } from "../../features/Cart/actions";
+import { menus } from "./menus";
+import { tags } from "./tags";
+import { BounceLoader } from "react-spinners";
+import { config } from "../../config";
+import TopBar from "../../components/TopBar";
+import Cart from "../../components/Cart";
+
+export default function Home() {
+  let dispatch = useDispatch;
+  let products = useSelector((state) => state.products);
+  let cart = useSelector((state) => state.cart);
+  let history = useHistory();
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [
+    dispatch,
+    products.currentPage,
+    products.keyword,
+    products.category,
+    products.tags,
+  ]);
+
+  return (
+    <div>
+      <LayoutSidebar
+        sidebar={
+          <SideNav
+            items={menus}
+            verticalAlign="top"
+            active={products.category}
+            onChange={(category) => dispatch(setCategory(category))}
+          />
+        }
+        content={
+          <div className="md:flex md:flex-row-reverse w-full mr-5 h-full min-h-screen">
+            <div className="w-full md:w-3/4 pl-5 pb-10">
+              <TopBar />
+              <div className="w-full text-center mb-10 mt-5">
+                <InputText
+                  fullRound
+                  value={products.keyword}
+                  placeholder="cari makanan favoritmu..."
+                  fitContainer
+                  onChange={(e) => {
+                    dispatch(setKeyword(e.taget.value));
+                  }}
+                />
+              </div>
+
+              <div className="mb-5 pl-2 flex w-3/3 overflow-auto pb-5">
+                {tags[products.category].map((tag, index) => {
+                  return (
+                    <div key={index}>
+                      <Pill
+                        text={tag}
+                        icon={tag.slice(0, 1).toUpperCase()}
+                        isActive={products.tags.includes(tag)}
+                        onClick={(_) => dispatch(toggleTag)}
+                      />
+                    </div>
+                  );
+                })}
+              </div>
+
+              {products.status === "proccess" && !products.data.length ? (
+                <div className="flex justify-center">
+                  <BounceLoader color="red" />
+                </div>
+              ) : null}
+
+              <Responsive desktop={3} items="stretch">
+                {products.data.map((product, index) => {
+                  return (
+                    <div key={index} className="p-2">
+                      <CardProduct
+                        title={products.name}
+                        imgUrl={`${config.api_host}/upload/${product.image_url}`}
+                        price={product.price}
+                        onAddToCart={(_) => dispatch(addItem(product))}
+                      />
+                    </div>
+                  );
+                })}
+              </Responsive>
+
+              <div className="text-center my-10">
+                <Pagination
+                  totalItems={products.totalItems}
+                  page={products.currentPage}
+                  perPage={products.perPage}
+                  onChange={(page) => dispatch(setPage(page))}
+                  onNext={(_) => dispatch(goToNextPage())}
+                  onPrev={(_) => dispatch(goToPrevPage())}
+                />
+              </div>
+            </div>
+
+            <div className="w-full md:w-1/4 h-full shadow-lg border-r border-white bg-gray-100">
+              <Cart
+                items={cart}
+                onItemInc={(item) => dispatch(addItem(item))}
+                onItemDec={(item) => dispatch(removeItem(item))}
+                onCheckout={(_) => history.push("/checkout")}
+              />
+            </div>
+          </div>
+        }
+        sidebarSize={80}
+      />
+    </div>
+  );
+}
