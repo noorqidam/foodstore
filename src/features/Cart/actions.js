8321921d7eada415dcdfc1e6483143819b425d73
noorqidam <noorqidam@gmail.com>
2020-12-24 11:36:19 +0700

    add actions Cart

---
+++ src/features/Cart/actions.js
@@ -0,0 +1,28 @@
+import { ADD_ITEM, CLEAR_ITEMS, REMOVE_ITEM, SET_ITEMS } from "./constants";
+
+export function addItem(item) {
+  return {
+    type: ADD_ITEM,
+    item,
+  };
+}
+
+export function removeItem(item) {
+  return {
+    type: REMOVE_ITEM,
+    item,
+  };
+}
+
+export function clearItems() {
+  return {
+    type: CLEAR_ITEMS,
+  };
+}
+
+export function setItems(items) {
+  return {
+    type: SET_ITEMS,
+    items,
+  };
+}
